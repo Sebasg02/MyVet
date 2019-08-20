a64614096ff56b922e0b5e24775873c22531b24c
SEBASTIAN GUISAO GALLEGO <sebastianguisao224908@correo.itm.edu.co>
2019-08-20 16:30:45 -0500

    Add User Entities

--- MyVet.Web/Data/DataContext.cs
+++ MyVet.Web/Data/DataContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MyVet.Web.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace MyVet.Web.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
@@ -14,6 +15,8 @@ namespace MyVet.Web.Data
         public DbSet<History> Histories { get; set; }
 
         public DbSet<Owner> Owners { get; set; }
+
+        public DbSet<Manager> Managers { get; set; }
 
         public DbSet<Pet> Pets { get; set; }
 
