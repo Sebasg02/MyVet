8a3c90409c36152ecc3654b27aee21392c273eab
SEBASTIAN GUISAO GALLEGO <sebastianguisao224908@correo.itm.edu.co>
2019-09-06 12:22:25 -0500

    Ligon with service

--- MyVet.Prism/MyVet.Prism/ViewModels/LoginPageViewModel.cs
+++ MyVet.Prism/MyVet.Prism/ViewModels/LoginPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Commands;
+﻿using MyVet.Common.Models;
+using MyVet.Common.Services;
+using Prism.Commands;
 using Prism.Navigation;
 
 namespace MyVet.Prism.ViewModels
@@ -6,14 +8,17 @@ namespace MyVet.Prism.ViewModels
     public class LoginPageViewModel : ViewModelBase
     {
         private readonly INavigationService _navigationService;
+        private readonly IApiService _apiService;
         private string _password;
         private bool _isRunning;
         private bool _isEnabled;
         private DelegateCommand _loginCommand;
 
-        public LoginPageViewModel(INavigationService navigationService) : base(navigationService)
+        public LoginPageViewModel(INavigationService navigationService,
+            IApiService apiService) : base(navigationService)
         {
             _navigationService = navigationService;
+            _apiService = apiService;
             Title = "My Vet - Login";
             IsEnabled = true;
         }
@@ -54,6 +59,27 @@ namespace MyVet.Prism.ViewModels
                 return;
             }
 
+            IsRunning = true;
+            IsEnabled = false;
+
+            var request = new TokenRequest
+            {
+                Password = Password,
+                Username = Email
+            };
+
+            var url = App.Current.Resources["UrlAPI"].ToString();
+            var response = await _apiService.GetTokenAsync(url, "/Account", "/CreateToken", request);
+
+            IsRunning = false;
+            IsEnabled = true;
+
+            if (!response.IsSuccess)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Email or password incorrect.", "Accept");
+                Password = string.Empty;
+                return;
+            }
             await App.Current.MainPage.DisplayAlert("Ok", "We are making progress!", "Accept");
         }
     }
